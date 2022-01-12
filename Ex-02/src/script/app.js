79a4811a82c70f639960a5a1083be69ac507c4b4
GbatZ2 <71204931+GbatZ2@users.noreply.github.com>
2022-01-12 11:06:56 +0100

    Add files via upload

--- Ex-02/src/script/app.js
+++ Ex-02/src/script/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const reset = document.getElementById("reset") ;
 const decrease = document.getElementById("decrease");
 const increase = document.getElementById("increase") ;
@@ -48,3 +49,55 @@ decrease.addEventListener("click", ()=>{
 });
 
 
+=======
+const reset = document.getElementById("reset") ;
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase") ;
+const numberDisplay = document.getElementById("number");
+let number = 0;
+
+numberDisplay.innerHTML = number;
+
+
+
+reset.addEventListener("click", ()=>{
+
+    number=0;
+
+    numberDisplay.textContent = number;
+    numberDisplay.classList.remove("negativeNumber");
+    numberDisplay.classList.remove("positiveNumber");
+});
+
+
+increase.addEventListener("click", ()=>{
+
+    number++;
+
+    numberDisplay.textContent = number;
+
+    if(number>0){
+        numberDisplay.classList.add("positiveNumber");
+        numberDisplay.classList.remove("negativeNumber");
+    }else if(number<0){
+        numberDisplay.classList.add("negativeNumber");
+        numberDisplay.classList.remove("positiveNumber");
+    }
+});
+
+decrease.addEventListener("click", ()=>{
+    number--;
+
+    numberDisplay.textContent = number;
+
+    if(number>0){
+        numberDisplay.classList.add("positiveNumber");
+        numberDisplay.classList.remove("negativeNumber");
+    }else if(number<0){
+        numberDisplay.classList.add("negativeNumber");
+        numberDisplay.classList.remove("positiveNumber");
+    }
+});
+
+
+>>>>>>> ccbc9512efe1abfd38b3464c1f5231d3af64623f
---
+++ Ex-03/src/script/app.js
@@ -0,0 +1,108 @@
+
+
+const reviews = [
+
+    {
+        id: 01,
+        name:"El Musico",
+        info:"No hace nada con su vida el tio, se piensa que la musica le va a servir pa algo pero na con suerte acaba en un Burger King q seria el goal más optimista para su vida",     
+        dream: "DJ",
+        image :"src/images/musico.jpeg"
+        
+    },
+    {
+        id: 02,
+        name:"El Personaje",
+        info:"El tio va de que es un personaje y tal pero todos sabemos que no, y encima ahora viene con dos conjones y hice quieres ser hacker a mos no me jodas",     
+        dream: "Ser el sustituto del Sesmesole",
+        image :"src/images/blackSuit.jpeg"
+    },
+    {
+        id: 03,
+        name:"El Nazi del Trabajo",
+        info:"Todos Sabemos que es bipolar y tal de repente te viene el tio con que es aficionado al clash royale como q te lo cambia por el golf, con suerte le da un infarto y se queda en el sitio",     
+        dream: "Dar otra espantada",
+        image :"src/images/elsuicidas.jpeg"
+    },
+    {
+        id: 04,
+        name:"Lorem2",
+        info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum qui ipsum quod, dignissimos, sequi similique in voluptate amet inventore labore sit quibusdam, aut natus soluta dolorum ipsa magnam nisi impedit!",     
+        dream: "Dar otra espantada",
+        image :"src/images/jacker.jpeg"
+    },  {
+        id: 05,
+        name:"Lorem3",
+        info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum qui ipsum quod, dignissimos, sequi similique in voluptate amet inventore labore sit quibusdam, aut natus soluta dolorum ipsa magnam nisi impedit!",     
+        dream: "Dar otra espantada",
+        image :"src/images/torneidou.jpeg"
+    }  ,  {
+        id: 06,
+        name:"Lorem4",
+        info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum qui ipsum quod, dignissimos, sequi similique in voluptate amet inventore labore sit quibusdam, aut natus soluta dolorum ipsa magnam nisi impedit!",     
+        dream: "Dar otra espantada",
+        image :"src/images/musico.jpeg"
+    }  ,  {
+        id: 07,
+        name:"dada",
+        info:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum qui ipsum quod, dignissimos, sequi similique in voluptate amet inventore labore sit quibusdam, aut natus soluta dolorum ipsa magnam nisi impedit!",     
+        dream: "Dar otra espantada",
+        image :"src/images/blackSuit2.0.jpeg"
+    }       
+
+];
+
+const title = document.querySelector(".name");
+const text = document.querySelector(".text");
+const name = document.querySelector(".name");
+const job = document.querySelector(".rol");
+const img = document.querySelector(".image");
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
+let btn = document.querySelector(".random");
+let contador =0;
+
+
+
+right.addEventListener("click",  () =>{
+    
+    if(contador===reviews.length){
+        contador=0;
+    }
+    
+    title.textContent =(reviews[contador].name);
+    job.textContent =(reviews[contador].dream);
+    text.textContent=(reviews[contador].info);
+    img.src=(reviews[contador].image);
+
+    contador++;
+
+    
+});
+
+left.addEventListener("click", ()=>{
+
+    if(contador>0){
+      
+        contador--;
+    }else{
+        contador=reviews.length;
+        contador--;
+    }
+    
+    title.textContent =(reviews[contador].name);
+    job.textContent =(reviews[contador].dream);
+    text.textContent=(reviews[contador].info);
+    img.src=(reviews[contador].image);
+    contador--;
+
+});
+
+btn.addEventListener("click", ()=>{
+
+    contador = Math.floor(Math.random()*reviews.length)
+    title.textContent =(reviews[contador].name);
+    job.textContent =(reviews[contador].dream);
+    text.textContent=(reviews[contador].info);
+    img.src=(reviews[contador].image);
+})
